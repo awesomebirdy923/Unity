ccafbc35a2c5437d620fdc04bb831dcac222bcd5
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-07-06 14:54:30 -0700

    hi
    hi

---
+++ idk/Assets/Scripts/DogeBox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeBox : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void FixedUpdate(){
+		gameObject.transform.Rotate (Vector3.up * speed);
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Player"){
+			GameObject.Destroy (gameObject);
+		}
+	}
+}
---
+++ idk/Assets/Scripts/LevelSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitcher : MonoBehaviour {
+
+	public string nextLevel;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.S)){
+			Debug.Log ("test");
+			SceneManager.LoadScene(0);
+		}
+	}
+}
