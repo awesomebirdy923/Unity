f15ef522ca309896ce2ffa8d9d5ca6f391dd73e0
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-07-08 14:57:36 -0700

    hi
    hi

--- idk/Assets/Scripts/EnemyController.cs
+++ idk/Assets/Scripts/EnemyController.cs
@@ -5,16 +5,26 @@ public class EnemyController : MonoBehaviour {
 
 	public GameObject player;
 	public float moveSpeed;
+	public int health;
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		//This will move the object toward the player GameObject
 		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);
 	}
-//	void onTriggerEnter(Collider c){
-//		if(c.gameObject.tag == "Player"){
+
+	void Update(){
+		if(health <= 0){
+			Destroy (gameObject);
+			Destroy (this);
+		}
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.gameObject.tag == "Projectile"){
 //			Destroy (gameObject);
 //			Destroy (this);
-//		}
-//	}
+			health--;
+		}
+	}
 } 
---
+++ idk/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthManager : MonoBehaviour {
+
+	public int health;
+
+	// Use this for initializationß
+	
+	// Update is called once per frame
+	void Update () {
+		if(health <= 0){
+			Destroy (gameObject);
+			Destroy (this);
+		}
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.gameObject.tag == "Enemy"){
+			health--;
+		}
+	}
+}
